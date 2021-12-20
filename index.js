04c16e4d360ac32afc0e209cda5d7dd47b823c76
Cecilia Daninthe <cecilia.daninthe@gmail.com>
2021-12-20 17:54:54 -0500

    commit tree 3284bca17944a3246f0ee9537e30107eeae65f51

---
+++ index.js
@@ -0,0 +1,11 @@
+function receivesAFunction (cb){
+    cb();
+}
+
+function returnsANamedFunction (){
+    return function aNamedFunction(){};
+}
+
+function returnsAnAnonymousFunction (){
+    return function (){};
+}
